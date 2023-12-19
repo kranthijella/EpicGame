48f54cda58cd67676243ca8eed237110ac86e2c5
Kranthi Jella <kjella@csuchico.edu>
2023-12-19 15:18:21 -0800

    logo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import {Grid, Show, GridItem} from '@chakra-ui/react'
+import NavBar from "./components/NavBar.tsx";
 
 function App() {
 
   return(
       <>
-          <Button colorScheme='blue'>Button</Button>
+
+          <Grid templateAreas = {{
+              base : `"nav"  "main"`,
+              lg : `"nav nav" "aside main"`
+          }}>
+                <GridItem area = 'nav' >
+                    <NavBar/>
+                </GridItem>
+                <Show above="lg">
+                    <GridItem area = 'aside' bg = 'gold'>aside</GridItem>
+                </Show>
+                <GridItem area = 'main' bg = 'dodgerblue'>main</GridItem>
+          </Grid>
       </>
   )
 }
